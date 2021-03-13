9d3afb4c7e4eaac95cf6fbdb216403b300346d75
Pau Egea <papau84@gmail.com>
2021-03-13 02:04:11 +0100

    mas Clases

--- src/com/P5/entities/Proyectos.java
+++ src/com/P5/entities/Proyectos.java
@@ -16,8 +16,79 @@ public class Proyectos {
 
     ArrayList<Personal>personal = new ArrayList<>();    //lista de personal que va trabajar en el proyecto
 
+    public Proyectos(String nombre, String pais, String localizacion, String lineAccion, String subLineaAccion, Date fechaInicio, Date fechaFin, String financiacionAportada, ArrayList<Personal> personal) {
+        this.nombre = nombre;
+        this.pais = pais;
+        this.localizacion = localizacion;
+        this.lineAccion = lineAccion;
+        this.subLineaAccion = subLineaAccion;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+        this.financiacionAportada = financiacionAportada;
+        this.personal = personal;
+    }
 
+    public String getNombre() {
+        return nombre;
+    }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
+    public String getPais() {
+        return pais;
+    }
 
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getLocalizacion() {
+        return localizacion;
+    }
+
+    public void setLocalizacion(String localizacion) {
+        this.localizacion = localizacion;
+    }
+
+    public String getLineAccion() {
+        return lineAccion;
+    }
+
+    public void setLineAccion(String lineAccion) {
+        this.lineAccion = lineAccion;
+    }
+
+    public String getSubLineaAccion() {
+        return subLineaAccion;
+    }
+
+    public void setSubLineaAccion(String subLineaAccion) {
+        this.subLineaAccion = subLineaAccion;
+    }
+
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public String getFinanciacionAportada() {
+        return financiacionAportada;
+    }
+
+    public void setFinanciacionAportada(String financiacionAportada) {
+        this.financiacionAportada = financiacionAportada;
+    }
 }
