2e4f60f9d4c521dd85e36c6508ceb62ce67cde32
Pau Egea <papau84@gmail.com>
2021-03-28 01:55:04 +0100

    Creando la implementacion de xml en personal

--- src/com/P5/XMLDAO/XMLPersonalDAO.java
+++ src/com/P5/XMLDAO/XMLPersonalDAO.java
@@ -3,13 +3,22 @@ package com.P5.XMLDAO;
 import com.P5.DAO.interfaces.IPersonal;
 import com.P5.entities.Personal;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import java.io.FileWriter;
 import java.util.List;
 
 public class XMLPersonalDAO implements IPersonal {
 
+    JAXBContext context = JAXBContext.newInstance(Personal.class);
+    Marshaller marshaller = context.createMarshaller();
+
 
     @Override
     public void insertar(Personal a) {
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.marshal(Personal, new FileWriter("Personal.xml"));
+
 
     }
 
